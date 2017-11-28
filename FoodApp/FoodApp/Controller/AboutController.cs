b0a29aa6b48916814f1329105d91646cd9b80840
Ajay Chauhan <ajaysinghc@gmail.com>
2017-11-28 18:23:09 +0530

    Attribute based routing

---
+++ FoodApp/FoodApp/Controller/AboutController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodApp.Controller
+{
+    [Route("[controller]")]
+    public class AboutController
+    {
+        [Route("")]
+        public string Phone()
+        {
+            return "1-22-33-33";
+        }
+        [Route("[Action]")]
+        public string Address()
+        {
+            return "India";
+        }
+    }
+}
